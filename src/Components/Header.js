3b1b544986552bf47ca07b70dea7fee39a5866e2
Sai Kumar K <>
2024-10-29 09:36:34 +0530

    auth removal changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,30 +1,11 @@
 import React, { useState } from 'react';
 import DarkModeToggle from './Darkmodetoggle';
-import { FaBars, FaTimes, FaHome, FaBlog, FaBook, FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
+import { FaBars, FaTimes, FaHome, FaBlog, FaBook} from 'react-icons/fa';
 import FlyoutMenu from './FlyoutMenu';
-import { useAuth0 } from '@auth0/auth0-react';
 import LogoSVG from '../assets/logo';
+
 const Header = () => {
     const [menuOpen, setMenuOpen] = useState(false);
-    const { isAuthenticated, loginWithRedirect, logout } = useAuth0()
-
-
-    const handleLogin = async () => {
-        await loginWithRedirect(
-            // {
-            //     appState: {
-            //         returnTo: "/profile",
-            //     },
-            // }
-        );
-    };
-    const handleLogout = () => {
-        logout({
-            logoutParams: {
-                returnTo: window.location.origin,
-            },
-        });
-    }
 
     return (
         <nav className="flex justify-between items-center p-4 bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-gray-100 transition duration-300">
@@ -43,9 +24,9 @@ const Header = () => {
                     </button>
                 </div>
                 {/* <div className="text-lg font-bold"> */}
-                    <div className=" flex items-center justify-center">
-                        <LogoSVG />
-                    </div>
+                <div className=" flex items-center justify-center">
+                    <LogoSVG />
+                </div>
                 {/* </div> */}
                 <div className="hidden md:block">|</div>
             </div>
@@ -53,31 +34,20 @@ const Header = () => {
             {/* Center on Desktop: Home */}
             <div className="hidden md:flex ml-4 items-center">
                 <FaHome className=" dark:text-gray-100 text-gray-600 space-x-2" />
-                <span className="hover:text-blue-400 ml-2 cursor-pointer">Home</span>
+                <span className="hover:text-blue-400 dark:hover:dark:text-teal-400 ml-2 cursor-pointer">Home</span>
             </div>
 
             {/* Right side contents (Blog, Samples, etc.) on Desktop */}
             <div className="md:flex ml-auto space-x-6 items-center">
                 <div className='hidden md:flex ml-auto space-x-6 items-center'>
-                    <div className="flex items-center space-x-2 hover:text-blue-400 cursor-pointer">
+                    <div className="flex items-center space-x-2 hover:text-blue-400 dark:hover:dark:text-teal-400 cursor-pointer">
                         <FaBlog className=" dark:text-gray-100 text-gray-600" />
                         <span>Blog</span>
                     </div>
-                    <div className="flex items-center space-x-2 hover:text-blue-400 cursor-pointer">
+                    <div className="flex items-center space-x-2 hover:text-blue-400 dark:hover:dark:text-teal-400 cursor-pointer">
                         <FaBook className=" dark:text-gray-100 text-gray-600" />
                         <span>Samples</span>
                     </div>
-                    {!isAuthenticated ? (
-                        <div className="flex items-center space-x-2 hover:text-blue-400 cursor-pointer" onClick={handleLogin}>
-                            <FaSignInAlt className=" dark:text-gray-100 text-gray-600" />
-                            <span>Login</span>
-                        </div>
-                    ) : (
-                        <div className="flex items-center space-x-2 hover:text-blue-400 cursor-pointer" onClick={handleLogout}>
-                            <FaSignOutAlt className=" dark:text-gray-100 text-gray-600" />
-                            <span>Logout</span>
-                        </div>
-                    )}
                 </div>
                 {/* Dark Mode Toggle */}
                 <div className="flex items-center">
@@ -87,7 +57,7 @@ const Header = () => {
 
 
             {/* Flyout Menu for Mobile */}
-            {menuOpen && <FlyoutMenu isAuthenticated={isAuthenticated} setMenuOpen={setMenuOpen} handleLogin={handleLogin} />}
+            {menuOpen && <FlyoutMenu setMenuOpen={setMenuOpen} handleLogin={handleLogin} />}
         </nav>
     );
 };
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { Auth0ProviderWithNavigate } from "./Components/Authprovider";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -11,9 +10,7 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <BrowserRouter>
-      <Auth0ProviderWithNavigate>
         <App />
-      </Auth0ProviderWithNavigate>
     </BrowserRouter>
   </StrictMode>
 );
